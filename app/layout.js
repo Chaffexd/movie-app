57f98399bced9ae358ec1802473c46196a88af74
Chaffexd <shanechaffe@gmail.com>
2023-10-22 18:49:22 +0100

    setup

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import MainNavigation from '@/components/Layout/MainNavigation'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -9,9 +10,13 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }) {
+  
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <MainNavigation />
+        <main>{children}</main>
+      </body>
     </html>
   )
 }
--- components/Layout/MainNavigation.js
+++ components/Layout/MainNavigation.js
@@ -1,11 +1,42 @@
-import classes from './mainnavigation.module.css';
+import IconNavHome from "@/assets/IconNavHome";
+import classes from "./mainnavigation.module.css";
+import IconNavMovies from "@/assets/IconNavMovies";
+import IconNavTVSeries from "@/assets/IconNavTVSeries";
+import IconNavBookmark from "@/assets/IconNavBookmark";
+import IconLogo from "@/assets/IconLogo";
+import Image from "next/image";
 
 const MainNavigation = () => {
   return (
-    <nav>
-        <ul></ul>
+    <nav className={classes.navBar}>
+      <ul className={classes.list}>
+        <div className={classes.listItems}>
+          <li className={classes.logo}>
+            <IconLogo />
+          </li>
+          <li>
+            <IconNavHome />
+          </li>
+          <li>
+            <IconNavMovies />
+          </li>
+          <li>
+            <IconNavTVSeries />
+          </li>
+          <li>
+            <IconNavBookmark />
+          </li>
+        </div>
+        <div>
+          <Image
+            src={"/movie-app/assets/image-avatar.png"}
+            width={40}
+            height={40}
+          />
+        </div>
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default MainNavigation
+export default MainNavigation;
