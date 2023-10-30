03aca5b9212d4bec9113ef5272c9665ba92cb2d0
Chaffexd <shanechaffe@gmail.com>
2023-10-30 17:46:21 +0000

    added css

--- components/Layout/Login.js
+++ components/Layout/Login.js
@@ -1,12 +1,14 @@
 import Link from "next/link";
 import classes from './mainnavigation.module.css'
+import IconLogo from "@/assets/IconLogo";
 
 const Login = () => {
   return (
-    <>
+    <div className={classes.loginContainer}>
+      <IconLogo />
       <h1 className={classes.title}>Sign up today</h1>
       <Link href="/api/auth/login" className={classes.login}>Login</Link>
-    </>
+    </div>
   );
 };
 
